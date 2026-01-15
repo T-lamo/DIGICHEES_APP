40fbcc1919ab9299cb39cb91c71f575cb25f3e8f
Amos Dorceus <amosdorceus2010@gmail.com>
2026-01-15 15:25:03 +0100

    chore: create db with sql model schema

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,5 @@
 from .exemple_model import Exemple, ExempleBase, ExempleRead, ExemplePatch
+from .schema_db_model import __all__ as schema_db_models_all
 
 
-__all__ = ["Exemple", "ExempleBase", "ExempleRead", "ExemplePatch"]
+__all__ = ["Exemple", "ExempleBase", "ExempleRead", "ExemplePatch"] + schema_db_models_all
