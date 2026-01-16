51eede6f20ab503c5b1a2aa3f231a6ab1837b288
obi-ann <anna.sytiuk@gmail.com>
2026-01-16 11:53:37 +0100

    ajout poids test swagger ok

--- src/models/poids_model.py
+++ src/models/poids_model.py
@@ -2,20 +2,19 @@ from typing import List, Optional
 from sqlmodel import SQLModel, Field
 from decimal import Decimal
 
-
-
 class PoidsBase(SQLModel):
-    libpoids: str | None = Field(default=None, max_length=50, nullable=True)
-    poidspoids: int | None = Field(default=None, nullable=True)
-    prixcond: Decimal = Field(default=Decimal("0.0000"), nullable=False)
-    ordreimp: int | None = Field(default=None, nullable=True)
+    min: Decimal | None = Field(default=Decimal("0"), nullable=True)
+    value: Decimal | None = Field(default=Decimal("0"), nullable=True)
     
 
 class PoidsPatch(PoidsBase):
-    prixcond: Decimal | None = None
+    min: Decimal | None = None
+    value: Decimal | None = None
 
 
 class PoidsRead(PoidsBase):
-    idpoids: int
+    id: int
+
+
 
 __all__ = ["PoidsBase", "PoidsRead", "PoidsPatch"]
