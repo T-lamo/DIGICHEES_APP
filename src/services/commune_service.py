1a9c2a74b8c620dc2d27b111fc2f6743cf656b3c
Amos Dorceus <amosdorceus2010@gmail.com>
2026-01-22 17:02:02 +0100

    fix(api): gestion d'erreur sur l'ajout et modification avec id departement

--- src/services/commune_service.py
+++ src/services/commune_service.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict, List
+from src.repositories.departement_repository import DepartementRepository
 from sqlmodel import Session
 from src.repositories.commune_repository import CommuneRepository
 from src.models import CommuneRead, Commune, CommuneBase, CommunePatch
@@ -7,6 +8,7 @@ from src.core import NotFoundException, BadRequestException
 class CommuneService:
     def __init__(self, db: Session):
         self.repo = CommuneRepository(db)
+        self.departement_repo = DepartementRepository(db)  # Placeholder for DepartementRepository if needed
 
     # ------------------------
     # LIST ALL
@@ -42,6 +44,9 @@ class CommuneService:
     def create_commune(self, data: CommuneBase) -> Commune:
         try:
             obj = Commune(**data.model_dump())
+            departement =  self.departement_repo.get_by_id(data.id_departement) if data.id_departement else None
+            if data.id_departement and not departement:
+                raise NotFoundException(f"Département {data.id_departement} introuvable")
             return self.repo.create(obj)
         except Exception as e:
             raise BadRequestException(str(e))
@@ -51,6 +56,9 @@ class CommuneService:
     # ------------------------
     def update_commune(self, idcommune: int, data: CommunePatch) -> Commune:
         obj = self.get_commune(idcommune)
+        departement =  self.departement_repo.get_by_id(data.id_departement) if data.id_departement else None
+        if data.id_departement and not departement:
+            raise NotFoundException(f"Département {data.id_departement} introuvable")
 
         update_data = data.model_dump(exclude_unset=True)
         for key, value in update_data.items():
